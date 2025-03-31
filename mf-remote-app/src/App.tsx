3094cfdd6234bd09863bfaae4d63fb20a12c6df0
iicefy <chawit_rittiya@hotmail.com>
2025-03-31 20:39:48 +0700

    init host and remote project

---
+++ mf-remote-app/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Provider from './components/ProviderComponent';
+
+const App = () => {
+  return (
+    <Provider />
+  );
+};
+
+export default App;
---
+++ mf-shell-app/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Provider from 'provider';
+
+const App = () => {
+  return (
+    <Provider />
+  );
+};
+
+export default App;
